aec9a280c94bed8dc1b20397e2c2c0b7669a595e
Legend0069 <98964334+BrandanDcBot@users.noreply.github.com>
2022-02-14 11:57:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const { SoundCloudPlugin } = require('@distube/soundcloud')
 
 client.config = require('./config.json')
 client.distube = new DisTube(client, {
+  youtubeCookie: VISITOR_INFO1_LIVE=U2QATraBYBM; PREF=tz=Asia.Katmandu&f6=40000000&f5=30000; LOGIN_INFO=AFmmF2swRQIgEFFHkukzOWBQMxoP1rJtpfcQcUrg1jgrGcE4GwAB-vkCIQD7-3xgs7ZVPFhxWYNCru-5am54qoN-Vmcia59iWygRGA:QUQ3MjNmeENNS2FSR2M1MjBXUlZGak5KbTZaUUlYRWRzRm5BT0M0YmloS3ZKZEYwOUtGZHZnQTdPYk5RSmkwbGVqWmhuSC1ON1ZTVFA5OFBsdGxJZHJucF9xRkV5Z054UkxfY1cxSWpLZmFQY1F5RXpDRFlGNEstSzRKYkR0ZXdfSkl3cHhFVnBpZ0s1dHdqaGJwQldic3FuUHlQLUlGWXln; SID=-wcVqRro67GFifmdlftZFeS5JEAl9jaWZyPVQCfw_7Q3Nsu7B0dr1r3seFK264Db58EsSA.; Secure-3PSID=-wcVqRro67GFifmdlftZFeS5JEAl9jaWZyPVQCfw_7Q3Nsu7pdxvk9qVm_kdi3E6inElYg.; HSID=AMOR6MIKosSG0sOAA; SSID=AEGlFwlmYV5AB3cC-; APISID=Y7Yo2SAxnIz4U1sl/A-3SZjry6vhyBgBQ8; SAPISID=vk9gOgIs4QfiInwL/A4hfXNFmDNi3PpWTA; Secure-3PAPISID=vk9gOgIs4QfiInwL/A4hfXNFmDNi3PpWTA; YSC=P0ojBQS_SMk; SIDCC=AJi4QfEVrKq60m8YEQ8qhRfoK0NMsi6FJkTAfn8F8VGc7hyLZvvbmW5py2eBSKk_1jKQgZ25X84; __Secure-3PSIDCC=AJi4QfH3aHjcC94EYd_hlT3GK6hJKuj7n6WFa-kGfynJm1WWEIbYtuIu9BiFBMcb9Hec6dvfY6Q
   leaveOnStop: false,
   emitNewSongOnly: true,
   emitAddSongWhenCreatingQueue: false,
@@ -43,7 +44,7 @@ fs.readdir('./commands/', (err, files) => {
 
 client.on('ready', () => {
   console.log(`${client.user.tag} is ready to play music.`)
-  client.user.setPresence({ activities: [{ name: "dsc.gg/brandan", type: `LISTENING` }], status: 'dnd' })
+  client.user.setPresence({ activities: [{ name: "!cmd", type: `LISTENING` }], status: 'idle' })
 })
 
 client.on('messageCreate', async message => {
