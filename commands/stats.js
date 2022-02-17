cc105ff9f96fb9c024f68be7f922e2a587c77f81
Legend0069 <98964334+BrandanDcBot@users.noreply.github.com>
2022-02-17 17:55:39 +0530

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -8,10 +8,20 @@ module.exports = {
     message.channel.send({
       embeds: [
         new Discord.MessageEmbed()
-          .setDescription(`**Servers: 100
-                           Users: ${client.users.cache.size}
-                           Ping ${client.ws.ping}ms**`)
-          .setColor('#ff0000')
+        .setColor("#ff000")
+        .setDescription(`Stats From Brandan`)
+        .setColor("#ff000")
+        .setDescription(`Stats From ${client.user.username}`)
+        .addField("⏳ Memory Usage", `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} / ${(os.totalmem() / 1024 / 1024).toFixed(2)} MB`)
+          .addField("⌚️ Uptime ", `${prettyMilliseconds(client.uptime)}`)
+          .addField("👥 Users", `${client.users.cache.size}`)
+          .addField("📁 Servers", `${client.guilds.cache.size}`)
+          .addField("🤖 Discord.js", `v${version}`)
+          .addField("👾 Node", `${process.version}`)
+          .addField("⚡ CPU", `\`\`\`md\n${os.cpus().map(i => `${i.model}`)[0]}\`\`\``)
+          .addField("✔️ Arch", `${os.arch()}`)
+          .addField("💻 Platform", `${os.platform()}`)
+          .addField("🖥 API Latency", `${(client.ws.ping)}ms`)  
       ]
     })
   }
