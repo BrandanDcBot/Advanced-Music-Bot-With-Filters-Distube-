d413fa6a45299b6a628f88146c0b1c17b64975b9
Legend0069 <98964334+BrandanDcBot@users.noreply.github.com>
2022-02-18 19:31:42 +0530

    Create 8d.js

---
+++ commands/8d.js
@@ -0,0 +1,19 @@
+const { Message, Client } = require("discord.js");
+
+module.exports = {
+  name: "8d",
+  aliases: ["8d"],
+  description: `set 8d filter in song`,
+  userPermissions: ['CONNECT'],
+  botPermissions: ['CONNECT'],
+  /**
+   *
+   * @param {Client} client
+   * @param {Message} message
+   * @param {String[]} args
+   */
+  run: async (client, message, args) => {
+    // code
+    message.reply(`working`)
+  },
+};
