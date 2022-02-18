9a2cdb9f1444f66d0f79f498fe95f3bba65aad8d
Legend0069 <98964334+BrandanDcBot@users.noreply.github.com>
2022-02-18 19:20:11 +0530

    Update filter.js

--- commands/filter.js
+++ commands/filter.js
@@ -5,8 +5,7 @@ module.exports = {
     run: async (client, message, args) => {
       const queue = client.distube.getQueue(message)
       if (!queue) return message.channel.send(`${client.emotes.error} | There is nothing in the queue right now!`)
-      if (args[0] === 'off' && queue.filters?.length) queue.setFilter(false),
-        { "Filter name"="Filter value"; "8d"="apulsator=hz=0.075" }
+      if (args[0] === 'off' && queue.filters?.length) queue.setFilter(false)
       else if (Object.keys(client.distube.filters).includes(args[0])) queue.setFilter(args[0])
       else if (args[0]) return message.channel.send(`${client.emotes.error} | Not a valid filter`)
       message.channel.send(`Current Queue Filter: \`${queue.filters.join(', ') || 'Off'}\``)
