f639b4a15f8830ec324c342ea5e7d29b0f0d390f
Legend0069 <98964334+BrandanDcBot@users.noreply.github.com>
2022-02-18 19:55:49 +0530

    Update 8d.js

--- commands/8d.js
+++ commands/8d.js
@@ -5,14 +5,14 @@ module.exports = {
     aliases: [],
 
     async execute(client, message) {
-        if (!message.member.voice.channel) return message.channel.send({embeds: [new MessageEmbed().setColor(client.color.error).setDescription(`You must be a voice channel before using this command.`)]});
+        if (!message.member.voice.channel) return message.channel.send({embeds: [new MessageEmbed().setColor("#ff0000").setDescription(`Plz Join A Voice Channel to use This command`)]});
 		if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send({embeds: [new MessageEmbed().setColor(client.color.error).setDescription(`You are not in same voice channel.`)]});
 		
         try {
             await client.distube.setFilter(message, '8d');
-            return message.channel.send({embeds: [new MessageEmbed().setColor(client.color.color).setDescription(`**3D** filter applying to the music.`)]});
+            return message.channel.send({embeds: [new MessageEmbed().setColor("#ff0000").setDescription(`**8D** filter applying to the music.`)]});
         } catch {
-            return message.channel.send({embeds: [new MessageEmbed().setColor(client.color.error).setDescription(`Currently there are no music is playing.`)]});
+            return message.channel.send({embeds: [new MessageEmbed().setColor("#ff0000").setDescription(`Currently there are no music is playing.`)]});
         }
     }
 }
